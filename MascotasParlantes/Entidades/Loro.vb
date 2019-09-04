c1235a6fa59ba012df424363fbd6c4809353cded
the penumbre <the_penumbre@hotmail.com>
2019-09-04 19:26:04 -0300

    TP POO completado hasta el punto 9

---
+++ MascotasParlantes/Entidades/Loro.vb
@@ -0,0 +1,64 @@
+﻿Imports System.DateTime
+Public Class Loro
+    Private _memoria As New Queue(Of String)
+    Private _memoria2 As String
+    Public _nombre As String
+    Public _edad As Short
+    Public _fechaNacimiento As Date
+
+    Public Property Nombre As String
+        Get
+            Return _nombre
+        End Get
+        Set(value As String)
+            _nombre = value
+        End Set
+    End Property
+    Public ReadOnly Property Edad As Short
+        Get
+            Return CalcularEdad(_fechaNacimiento)
+        End Get
+    End Property
+    Public Property FechaNacimiento As Date
+        Get
+            Return _fechaNacimiento
+        End Get
+        Set(value As Date)
+            _fechaNacimiento = value
+        End Set
+    End Property
+
+    Public Sub New()
+        Nombre = ""
+        FechaNacimiento = #07/20/2017#
+    End Sub
+
+    Private Function CalcularEdad(fechaNacimiento As Date) As Short
+        Dim diasFechaNacimiento As Short = (fechaNacimiento.DayOfYear)
+        Dim diasActules As Short = (Date.Today.DayOfYear)
+        Dim anio As Short = (Date.Today.Year) - (fechaNacimiento.Year)
+        Dim dias As Short = diasActules - diasFechaNacimiento
+
+        If ((fechaNacimiento.Year Mod 4 = 0 And fechaNacimiento.Year Mod 100 <> 0 Or fechaNacimiento.Year Mod 400 = 0) And diasFechaNacimiento >= 60) Then
+            dias += 1
+        End If
+        If ((Date.Today.Year Mod 4 = 0 And Date.Today.Year Mod 100 <> 0 Or Date.Today.Year Mod 400 = 0) And diasActules >= 60) Then
+            dias -= 1
+        End If
+
+        If dias < 0 Then
+            Return anio - 1
+        Else
+            Return anio
+        End If
+    End Function
+
+    Public Sub Escuchar(frase As String)
+        _memoria.Enqueue(frase)
+    End Sub
+
+    Public Function Hablar() As String
+        _memoria2 &= _memoria.Dequeue()
+        Return _memoria2
+    End Function
+End Class
---
+++ MascotasParlantes/EntidadesTest/LoroTest.vb
@@ -0,0 +1,22 @@
+Imports System
+Imports Entidades
+
+Module LoroTest
+    Sub Main()
+        Dim loro1 As New Loro()
+        Console.WriteLine("Nombre: " & loro1.Nombre)
+        Console.WriteLine("Edad: " & loro1.Edad)
+        Console.WriteLine("Fecha de Nacimiente: " & loro1.FechaNacimiento)
+        loro1.Nombre = "PEPE"
+        loro1.FechaNacimiento = Date.Today
+        Console.WriteLine("Nombre: " & loro1.Nombre)
+        Console.WriteLine("Edad: " & loro1.Edad)
+        Console.WriteLine("Fecha de Nacimiente: " & loro1.FechaNacimiento)
+        loro1.Escuchar("Papita")
+        loro1.Escuchar("pa el loro ")
+        loro1.Escuchar("Juan")
+        For index = 1 To 3
+            Console.WriteLine(loro1.Hablar)
+        Next
+    End Sub
+End Module
